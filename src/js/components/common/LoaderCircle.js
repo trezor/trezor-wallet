cd4d7ed6b1cb1af09d81a78b95715c2ec33c98c4
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-08-02 15:19:26 +0200

    Show loader after 'Acquire Device' button is clicked and is waiting for acquire

--- src/js/components/common/LoaderCircle.js
+++ src/js/components/common/LoaderCircle.js
@@ -3,14 +3,22 @@
 
 import React from 'react';
 
-export default (props: { size: string, label?: string }): React$Element<string> => {
+type Props = {
+    size: string,
+    label?: string,
+    className?: string,
+};
+
+export default (props: Props): React$Element<string> => {
+    const className = props.className ? `loader-circle ${props.className}` : 'loader-circle';
+
     const style = {
         width: `${props.size}px`,
         height: `${props.size}px`,
-    };
+    }
 
     return (
-        <div className="loader-circle" style={style}>
+        <div className={ className } style={ style }>
             <p>{ props.label }</p>
             <svg className="circular" viewBox="25 25 50 50">
                 <circle className="route" cx="50" cy="50" r="20" fill="none" stroke="" strokeWidth="1" strokeMiterlimit="10" />
--- src/js/components/wallet/pages/Acquire.js
+++ src/js/components/wallet/pages/Acquire.js
@@ -23,16 +23,32 @@ const Acquire = (props: Props) => {
             },
         },
     ];
+    const acquireDeviceAction = {
+        label: 'Acquire device',
+        callback: () => {
+            props.acquireDevice()
+        }
+    };
 
     return (
         <section className="acquire">
-            <Notification
-                title="Device is used in other window"
-                message="Do you want to use your device in this window?"
-                className="info"
-                cancelable={false}
-                actions={actions}
-            />
+            {props.acquiring ? (
+                <Notification
+                    title="Device is being acquired"
+                    message="Please wait"
+                    className="loading"
+                    cancelable={false}
+                    loading={true}
+                />
+            ) : (
+                <Notification
+                    title="Device is used in other window"
+                    message="Do you want to use your device in this window?"
+                    className="info"
+                    cancelable={ false }
+                    actions={ [acquireDeviceAction] }
+                />
+            )}
         </section>
     );
 };
