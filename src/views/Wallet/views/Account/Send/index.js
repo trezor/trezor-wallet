2e74b20eaad78b6a3596e74a282316c1a36126d3
Szymon Lesisz <sz.lesisz@gmail.com>
2018-11-27 11:44:23 +0100

    rename coin specific components

--- src/views/Wallet/views/Account/Send/index.js
+++ src/views/Wallet/views/Account/Send/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import type { State } from 'flowtype';
-import EthereumSend from './ethereum/Container';
-import RippleSend from './ripple/Container';
+import EthereumTypeSendForm from './ethereum/Container';
+import RippleTypeSendForm from './ripple/Container';
 
 export type BaseProps = {
     selectedAccount: $ElementType<State, 'selectedAccount'>,
@@ -19,9 +19,9 @@ export default connect((state: State): BaseProps => ({
 
     switch (network.type) {
         case 'ethereum':
-            return <EthereumSend />;
+            return <EthereumTypeSendForm />;
         case 'ripple':
-            return <RippleSend />;
+            return <RippleTypeSendForm />;
         default:
             return null;
     }
