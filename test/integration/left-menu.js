30ba930772081e485f270318a03dfc0ba4bccbdc
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-01-16 15:11:13 +0100

    First POC test in gitlab

--- test/integration/left-menu.js
+++ test/integration/left-menu.js
@@ -1,8 +1,7 @@
 describe('Left Menu', () => {
-    beforeEach(() => {
-        cy.visit('https://localhost:8080/#/');
-        cy.getTestElement('Modal__disclaimer__button__confirm').click();
-    });
+    // beforeEach(() => {
+    //     cy.getTestElement('Modal__disclaimer__button__confirm').click();
+    // });
 
     it('divider', () => {
         cy.getTestElement('Main__page__coin__menu__divider')
