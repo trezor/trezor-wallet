f3f6c0d6906cdc470aa11ae728b4b61a6b71a732
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-01-30 17:02:16 +0100

    test 17

--- test/scripts/run-all.sh
+++ test/scripts/run-all.sh
@@ -11,10 +11,10 @@ cd /trezor-bridge && ./extracted/usr/bin/trezord -ed 21324:21325 -u=false &
 cd /trezor-emulator/trezor-core && PYOPT=0 ./emu.sh &
 
 # run wallet
-cd /trezor-wallet && yarn run server:stable &
+cd /trezor-wallet && PORT=8000 yarn run server:stable &
 
 # init device
 npx babel-node /trezor-wallet/test/scripts/init-device.js &
 
 # run tests
-yarn run test-integration:gitlab -c baseUrl="https://localhost:8080/#/"
+yarn run test-integration:gitlab -c baseUrl="https://localhost:8000/#/"
