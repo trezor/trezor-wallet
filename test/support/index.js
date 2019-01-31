b70e20acb7a458b32b0a8c6b166857c28dcf0ee9
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-01-31 13:53:32 +0100

    test 23

--- test/support/index.js
+++ test/support/index.js
@@ -1,8 +1,10 @@
 import 'babel-polyfill';
 import './commands';
 
-cy.server({
-    onAnyRequest: function (route,  proxy) {
-        proxy.xhr.setRequestHeader('Origin',  'https://wallet.trezor.io');
-    }
-});
+beforeEach(() => {
+    cy.server({
+        onAnyRequest: function (route,  proxy) {
+            proxy.xhr.setRequestHeader('Origin',  'https://wallet.trezor.io');
+        }
+    });
+})
