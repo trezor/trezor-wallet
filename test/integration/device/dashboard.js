b2b522bba282b208c7a3856bcddb8ce6390f5658
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-02-01 15:20:46 +0100

    turn on debug

---
+++ test/integration/device/dashboard.js
@@ -0,0 +1,12 @@
+describe('Not initialized page', () => {
+    beforeEach(() => {
+        cy.viewport(1366, 768);
+        cy.visit('/');
+    });
+
+    it('header', () => {
+        cy.getTestElement('Main__page__device__header')
+            .should('be.visible')
+            .matchImageSnapshot();
+    });
+});
